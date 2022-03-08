5da473056f7e638b06b3b28d3b6901edda157299
Maxim Kotlyarenko <s1mple.eq.easy@gmail.com>
2022-03-08 20:07:37 +0300

    joystick added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,15 +6,15 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour 
 {
-
+    public bool controllWithJoystick = false;
     public float playerHealth = 100;
 	public float speed = 2f;
 	public float acceleration = 6f;
 	public float staminaFill = 1f;
 	public GameObject dieMessage;
 	public GameObject flashlight;
-	public Rigidbody2D rb;
 	public Camera cam;
+	public Joystick joystick;
 	public Image bar;
 	public Image stamina;
 	public AudioClip walkSound;
@@ -23,15 +23,18 @@ public class PlayerController : MonoBehaviour
 	private float healthFill;
 	public float rotationOffset;
 	private GameObject reloadNotifier;
+	private Rigidbody2D rb;
 	private bool flashlightIsActive;
 	Vector2 movement;
 	Vector2 mousePos;
+	Vector2 moveVelocity;
 	AudioSource playerAudioSrs;
 	void Start()
 	{
 		flashlightIsActive = false;
 		reloadNotifier = GetComponentInChildren<GunController>().reloadNotification;
         playerAudioSrs = GetComponent<AudioSource>();
+		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void Update () 
@@ -64,9 +67,49 @@ public class PlayerController : MonoBehaviour
            playerAudioSrs.PlayOneShot(fleshlightSound);
 		   flashlightIsActive = false;
 		}
+
+		//if(joystick.Horizontal > 0){transform.localRotation = Quaternion.Euler(0,0,0);}
+		//if(joystick.Horizontal < 0){transform.localRotation = Quaternion.Euler(0,100,0);}
+		Vector2 moveInput = new Vector2 (joystick.Horizontal, joystick.Vertical);
+		moveVelocity = moveInput.normalized * speed;
 	}
 	void FixedUpdate ()
 	{
+		if(controllWithJoystick != true)
+		{
+		if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.A))
+		{
+		   if(Input.GetKey(KeyCode.LeftShift))
+		   {
+			   if(staminaFill >0)
+			   {
+			    	rb.MovePosition(rb.position +  movement * acceleration * Time.fixedDeltaTime);
+			        staminaFill -= 0.002f;
+			   }
+			   else
+			   {
+			    	rb.MovePosition(rb.position +  movement * speed * Time.fixedDeltaTime);
+			   }
+		   }
+	
+		   else
+		   {
+			   if(staminaFill<1)
+			   {
+		         staminaFill +=0.001f;
+		       }
+		   rb.MovePosition(rb.position +  movement * speed * Time.fixedDeltaTime);
+		   }
+		}
+		else if(staminaFill < 1)
+		{
+		   staminaFill +=0.004f;
+		}
+		}
+		else
+		{
+            rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
+		}
 
 		mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 		
